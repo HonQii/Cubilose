e0ce5ae7f64dfdb2ed834b14aeeddf1325775983
honqi <honqi3014@gmail.com>
2020-05-16 19:39:40 +0800

    Initialize project

---
+++ Cubilose/Cubilose.h
@@ -0,0 +1,19 @@
+//
+//  Cubilose.h
+//  Cubilose
+//
+//  Created by HonQi on 2020/5/16.
+//  Copyright © 2020 HonQi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Cubilose.
+FOUNDATION_EXPORT double CubiloseVersionNumber;
+
+//! Project version string for Cubilose.
+FOUNDATION_EXPORT const unsigned char CubiloseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Cubilose/PublicHeader.h>
+
+
